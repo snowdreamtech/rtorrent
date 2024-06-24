4db0a7b0ea3872c19c413894716318f4e5482020
snowdream <sn0wdr1am@qq.com>
2024-06-24 11:26:27 +0800

    feat:  remove support for linux/riscv64

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ DOCKER_HUB_PROJECT=snowdreamtech/rtorrent
 
 GITHUB_PROJECT=ghcr.io/snowdreamtech/rtorrent
 
-docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
+docker buildx build --platform=linux/386,linux/amd64,linux/arm64,linux/ppc64le,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:flood-latest \
 -t ${DOCKER_HUB_PROJECT}:flood-3.20.0 \
 -t ${DOCKER_HUB_PROJECT}:flood-3.20 \
