81007531f1df3283eb3846fc3ac39666b8f3efea
snowdream <sn0wdr1am@qq.com>
2024-06-24 13:04:30 +0800

    fix: rtorrent version

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ GITHUB_PROJECT=ghcr.io/snowdreamtech/rtorrent
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:3.20.0 \
--t ${DOCKER_HUB_PROJECT}:3.20 \
--t ${DOCKER_HUB_PROJECT}:3 \
+-t ${DOCKER_HUB_PROJECT}:0.9.8 \
+-t ${DOCKER_HUB_PROJECT}:0.9 \
+-t ${DOCKER_HUB_PROJECT}:0 \
 -t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:3.20.0 \
--t ${GITHUB_PROJECT}:3.20 \
--t ${GITHUB_PROJECT}:3 \
+-t ${GITHUB_PROJECT}:0.9.8 \
+-t ${GITHUB_PROJECT}:0.9 \
+-t ${GITHUB_PROJECT}:0 \
 . \
 --push
