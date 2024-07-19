5eadc3d36892581ea7a9e930b24855eaa4be8e18
snowdream <sn0wdr1am@qq.com>
2024-07-19 11:24:44 +0800

    feat: build.sh

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ DOCKER_HUB_PROJECT=snowdreamtech/rtorrent
 GITHUB_PROJECT=ghcr.io/snowdreamtech/rtorrent
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/s390x \
--t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:0.9.8 \
--t ${DOCKER_HUB_PROJECT}:0.9 \
--t ${DOCKER_HUB_PROJECT}:0 \
--t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:0.9.8 \
--t ${GITHUB_PROJECT}:0.9 \
--t ${GITHUB_PROJECT}:0 \
-. \
---push
+    -t ${DOCKER_HUB_PROJECT}:latest \
+    -t ${DOCKER_HUB_PROJECT}:0.9.8 \
+    -t ${DOCKER_HUB_PROJECT}:0.9 \
+    -t ${DOCKER_HUB_PROJECT}:0 \
+    -t ${GITHUB_PROJECT}:latest \
+    -t ${GITHUB_PROJECT}:0.9.8 \
+    -t ${GITHUB_PROJECT}:0.9 \
+    -t ${GITHUB_PROJECT}:0 \
+    . \
+    --push
