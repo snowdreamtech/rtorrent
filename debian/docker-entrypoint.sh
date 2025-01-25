18780c4f1491f7fb0fefd5c9a243aa027f59c8e6
snowdreamtech <snowdreamtech@qq.com>
2025-01-25 15:35:06 -0500

    feat: rtorrent

--- debian/docker-entrypoint.sh
+++ debian/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# set RPC_PORT
+if [ -n "${RPC_PORT}" ]; then
+    sed -i "s/network\.port_range\.set.*/network\.port_range\.set = ""${RPC_PORT}""-""${RPC_PORT}""/g" /var/lib/rtorrent/config/rtorrent.rc
+fi
+
+# rtorrent
+/usr/bin/rtorrent -n -o import=/var/lib/rtorrent/config/rtorrent.rc
+
 # exec commands
 if [ -n "$*" ]; then
     sh -c "$*"
